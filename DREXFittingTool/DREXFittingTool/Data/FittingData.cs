28b344cf1a229cd7f180eb7ea3b333b6064c4063
Mai Tien Thang <thangmtdev@gmail.com>
2021-10-13 09:00:53 +0700

    d

---
+++ DREXFittingTool/DREXFittingTool/Data/FittingData.cs
@@ -0,0 +1,23 @@
+﻿using Autodesk.Revit.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DREXFittingTool.Data
+{
+    public class FittingData
+    {
+        public FamilySymbol m_symbol;
+        public string m_markFitting;
+        public double m_height;
+
+        public FittingData(FamilySymbol fsymbol, string markFitting, double height)
+        {
+            m_symbol = fsymbol;
+            m_markFitting = markFitting;
+            m_height = height;
+        }
+    }
+}
