f396527e997915ef60d86454c28b3f3cf8e07604
Mai Tien Thang <thangmtdev@gmail.com>
2021-10-13 08:43:39 +0700

    Update new source code

---
+++ DREXFittingTool/DREXFittingTool/UI/FormUpdateFitting.cs
@@ -0,0 +1,87 @@
+﻿using Autodesk.Revit.DB;
+using DREXFittingTool.Utils;
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace DREXFittingTool.UI
+{
+    public partial class FormUpdateFitting : System.Windows.Forms.Form
+    {
+        public bool IsFitting = false;
+        public double m_Height = 0;
+        public double m_Width = 0;
+        public string m_SelectedViewName;
+
+        public FormUpdateFitting(List<string> viewDrafts)
+        {
+            InitializeComponent();
+            this.Text = "DREXUpdateFittingTool";
+            cbxView.DataSource = viewDrafts;
+        }
+
+        private void FormFitting_Load(object sender, EventArgs e)
+        {
+            radFitting.Checked = true;
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            if (txtWidth.Text.Trim() == "")
+            {
+                Common.ShowError("横改行幅を入力してください。");
+                return;
+            }
+
+            if (txtHeight.Text.Trim() == "")
+            {
+                Common.ShowError("縦改ページ幅を入力してください。");
+                return;
+            }
+
+            if (radFitting.Checked)
+                IsFitting = true;
+            else
+                IsFitting = false;
+
+            if (Convert.ToInt32(txtWidth.Text) == 0 || Convert.ToInt32(txtHeight.Text) == 0)
+            {
+                Common.ShowError("横改行幅と縦改ページ幅は0より大きくする必要があります。");
+                return;
+            }
+
+            m_Height = Convert.ToDouble(txtHeight.Text) / 304.8;
+            m_Width = Convert.ToDouble(txtWidth.Text) / 304.8;
+            m_SelectedViewName = cbxView.SelectedItem.ToString();
+            this.DialogResult = DialogResult.OK;
+        }
+
+        private void txtRow_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+
+            // only allow one decimal point
+            if ((e.KeyChar == '.') && ((sender as System.Windows.Forms.TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void txtCol_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+
+            // only allow one decimal point
+            if ((e.KeyChar == '.') && ((sender as System.Windows.Forms.TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
---
+++ DREXFittingTool/DREXFittingTool/Utils/Define.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DREXFittingTool.Utils
+{
+    internal class Define
+    {
+        public static string FamilyName = "接手リスト項目_開発用暫定_タイトル幅追加_20210913b";
+        public static string ViewName = "継手リスト";
+    }
+}
